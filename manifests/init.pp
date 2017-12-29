9919908f5f6a0707dd4ddb76991cb8c86d3aee80
Jordi Prats <jprats@systemadmin.es>
2017-12-29 17:38:35 +0100

    notes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,29 @@
+# # Use this command if a webserver is already running with the webroot
+# # at /var/www/html.
+# certbot-auto certonly \
+#   --agree-tos \
+#   --non-interactive \
+#   --text \
+#   --rsa-key-size 4096 \
+#   --email admin@example.com \
+#   --webroot-path /var/www/html \
+#   --domains "example.com, www.example.com"
+#
+# # Use this command if no webserver is running. Certbot will launch its
+# # own webserver during the generation process.
+# certbot-auto certonly \
+#   --standalone \
+#   --agree-tos \
+#   --non-interactive \
+#   --text \
+#   --rsa-key-size 4096 \
+#   --email admin@example.com \
+#   --domains "example.com, www.example.com"
+
 class letsencrypt(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits letsencrypt::params{
+                    $manage_package = true,
+                    $package_ensure = 'installed',
+                  ) inherits letsencrypt::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,15 @@ class letsencrypt::install inherits letsencrypt {
 
   if($letsencrypt::manage_package)
   {
+    if($fping::params::include_epel)
+    {
+      include ::epel
+
+      Package[$fping::params::package_name] {
+        require => Class['::epel'],
+      }
+    }
+
     package { $letsencrypt::params::package_name:
       ensure => $letsencrypt::package_ensure,
     }
