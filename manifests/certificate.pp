f6ebbb466246d4c0858128e255d2009ee8989d40
Jordi Prats <jprats@systemadmin.es>
2018-01-11 18:57:53 +0100

    cert

---
+++ manifests/certificate.pp
@@ -0,0 +1,20 @@
+# cert = /etc/letsencrypt/live/example.com/cert.pem
+# privkey = /etc/letsencrypt/live/example.com/privkey.pem
+# chain = /etc/letsencrypt/live/example.com/chain.pem
+# fullchain = /etc/letsencrypt/live/example.com/fullchain.pem
+define letsencrypt::certificate (
+                                  $domains = [],
+                                ) {
+  #
+  $cert_file = inline_template('/etc/letsencrypt/live/<%= @domains.first %>/cert.pem')
+  $conf_file = inline_template('/etc/letsencrypt/renew/<%= @domains.first %>.conf')
+
+
+  file { $conf_file:
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/cert.erb")
+  }
+}
