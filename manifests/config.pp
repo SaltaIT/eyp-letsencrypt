adea4f114f7a3aab17660f69d41ff05714584783
Jordi Prats <jprats@systemadmin.es>
2018-01-11 13:13:42 +0100

    config

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,4 +6,14 @@ class letsencrypt::config inherits letsencrypt {
     group  => 'root',
     mode   => '0755',
   }
+
+  # /etc/letsencrypt/cli.ini
+  file { '/etc/letsencrypt/cli.ini':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/cli.erb"),
+    require => File['/etc/letsencrypt'],
+  }
 }
