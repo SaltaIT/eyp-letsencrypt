b5f93f22ccb6f2fd567d500c89b8f1071d80b6e4
Jordi Prats <jordi.prats@gmail.com>
2018-04-18 13:53:05 +0200

    certdir

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -9,12 +9,23 @@ define letsencrypt::certificate (
                                 ) {
   #
   $conf_file = inline_template('/etc/letsencrypt/renewal/<%= @domains.first %>.conf')
+  $cert_dir = inline_template('/etc/letsencrypt/live/<%= @domains.first %>')
+
+  #
   $cert_file = inline_template('/etc/letsencrypt/live/<%= @domains.first %>/cert.pem')
   $privkey_file = inline_template('/etc/letsencrypt/live/<%= @domains.first %>/privkey.pem')
   $chain_file = inline_template('/etc/letsencrypt/live/<%= @domains.first %>/chain.pem')
   $fullchain_file = inline_template('/etc/letsencrypt/live/<%= @domains.first %>/fullchain.pem')
   $archive_dir = inline_template('/etc/letsencrypt/archive/<%= @domains.first %>')
-  
+
+  file { $cert_dir:
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Class['::letsencrypt'],
+  }
+
   file { $conf_file:
     ensure  => 'present',
     owner   => 'root',
