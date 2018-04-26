7b59902fc75253479f09bb9330d72ed4e9ac62b6
Jordi Prats <jordi.prats@gmail.com>
2018-04-26 14:51:53 +0200

    lint

--- examples/demo.pp
+++ examples/demo.pp
@@ -1,7 +1,7 @@
 
 class { 'letsencrypt':
-agree_tos             => true,
-unsafe_registration   => true,
+  agree_tos           => true,
+  unsafe_registration => true,
 }
 
 letsencrypt::certificate { 'letstest.systemadmin.es':
