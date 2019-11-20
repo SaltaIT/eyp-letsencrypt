9b37599faa720e438dad8d5558ef2b84a42debba
jordiprats <jordi.prats@gmail.com>
2019-11-20 22:51:08 +0100

    varis

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,5 @@
 class letsencrypt::service inherits letsencrypt {
 
-  #
-  validate_bool($letsencrypt::manage_docker_service)
-  validate_bool($letsencrypt::manage_service)
-  validate_bool($letsencrypt::service_enable)
-
-  validate_re($letsencrypt::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${letsencrypt::service_ensure}")
-
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
